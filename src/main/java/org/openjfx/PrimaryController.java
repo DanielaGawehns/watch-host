74731c0e43080d677040a7eb02eec7850386cc8c
William Corsel <william99@live.nl>
2019-09-24 12:51:17 +0200

    ff wat leuker

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,12 +1,35 @@
 package org.openjfx;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private LineChart<Number, Number> sensorChart;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        final XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(1, 23));
+        series.getData().add(new XYChart.Data<>(2, 14));
+        series.getData().add(new XYChart.Data<>(3, 15));
+        series.getData().add(new XYChart.Data<>(4, 24));
+        series.getData().add(new XYChart.Data<>(5, 34));
+        series.getData().add(new XYChart.Data<>(6, 36));
+        series.getData().add(new XYChart.Data<>(7, 22));
+        series.getData().add(new XYChart.Data<>(8, 45));
+        series.getData().add(new XYChart.Data<>(9, 43));
+        series.getData().add(new XYChart.Data<>(10, 17));
+        series.getData().add(new XYChart.Data<>(11, 29));
+        series.getData().add(new XYChart.Data<>(12, 25));
+
+        sensorChart.getData().add(series);
     }
 }
