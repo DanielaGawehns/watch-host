0f7bc4400ae64fa9c08b3002835e4214b79c4e9b
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2019-11-18 23:52:33 +0100

    Connection: protocol version handshake

--- src/main/java/nl/liacs/watch/protocol/server/Connection.java
+++ src/main/java/nl/liacs/watch/protocol/server/Connection.java
@@ -1,8 +1,10 @@
 package nl.liacs.watch.protocol.server;
 
 import nl.liacs.watch.protocol.types.Message;
+import nl.liacs.watch.protocol.types.UnknownProtocolException;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -11,13 +13,28 @@ import java.net.Socket;
  */
 public class Connection implements nl.liacs.watch.protocol.Connection {
     private final Socket socket;
+    private final DataInputStream is;
+    private final DataOutputStream os;
 
     /**
-     * Create a new Connection using the given socket.
+     * Create a new Connection using the given socket and does a version handshake.
      * @param socket The socket to wrap.
+     * @throws IOException IO error when failing to read from or send to the socket.
+     * @throws UnknownProtocolException When the protocol version is unknown to the server.
      */
-    public Connection(Socket socket) {
+    public Connection(Socket socket) throws IOException, UnknownProtocolException {
         this.socket = socket;
+        this.is = new DataInputStream(socket.getInputStream());
+        this.os = new DataOutputStream(socket.getOutputStream());
+
+        // handshake
+        var version = this.is.readUnsignedByte();
+        if (version != 1) { // simple version check
+            this.close();
+            throw new UnknownProtocolException("watch runs unknown protocol version", version);
+        }
+
+        this.os.writeByte(0b00000110); // send ACK.
     }
 
     /**
@@ -33,8 +50,7 @@ public class Connection implements nl.liacs.watch.protocol.Connection {
      */
     @Override
     public Message receive() throws IOException {
-        var stream = new DataInputStream(this.socket.getInputStream());
-        return Message.decode(stream);
+        return Message.decode(this.is);
     }
 
     /**
@@ -42,7 +58,7 @@ public class Connection implements nl.liacs.watch.protocol.Connection {
      */
     @Override
     public void send(Message message) throws Exception {
-        this.socket.getOutputStream().write(message.encode());
+        this.os.write(message.encode());
     }
 
     /**
@@ -50,6 +66,8 @@ public class Connection implements nl.liacs.watch.protocol.Connection {
      */
     @Override
     public void close() throws IOException {
+        this.is.close();
+        this.os.close();
         this.socket.close();
     }
 }
---
+++ src/main/java/nl/liacs/watch/protocol/types/UnknownProtocolException.java
@@ -0,0 +1,19 @@
+package nl.liacs.watch.protocol.types;
+
+public class UnknownProtocolException extends Exception {
+    private final int protocolVersion;
+
+    public UnknownProtocolException(String message, int protocolVersion) {
+        super(message);
+        this.protocolVersion = protocolVersion;
+    }
+
+    public int getProtocolVersion() {
+        return protocolVersion;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + " (protocol version " + this.protocolVersion + ")";
+    }
+}
