3f8b6ab21eb35f37240d86cea87f16d27b3a3d08
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2019-11-26 13:01:18 +0100

    build connection manager on app startup

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -5,14 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import nl.liacs.watch.protocol.server.ConnectionManager;
+import nl.liacs.watch.protocol.tcpserver.Server;
+import nl.liacs.watch.protocol.types.Constants;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main class for loading the main screen
  */
 public class App extends Application {
-
+    /**
+     * The global connection manager for watch communication.
+     */
+    static ConnectionManager connectionManager;
+    /**
+     * @return The global connection manager for watch communication.
+     */
+    public static ConnectionManager GetConnectionManager() {
+        return connectionManager;
+    }
 
     /**
      * Start function
@@ -42,7 +56,11 @@ public class App extends Application {
      * Main function
      * @param args Unused launch arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        var server = Server.createServer(Constants.TcpPort);
+        Logger.getGlobal().log(Level.INFO, "running tcp server on port " + Constants.TcpPort);
+        connectionManager = new ConnectionManager(server);
+
         launch();
     }
 
